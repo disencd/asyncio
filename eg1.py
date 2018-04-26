11f349345f9b21ef203051a218e3e5823d1ecc70
Disen Chitilapilly <disen.chitilapilly@ooma.com>
2018-04-26 10:59:26 -0700

    Sample code

---
+++ eg1.py
@@ -0,0 +1,18 @@
+import asyncio
+
+async def speak_async():
+    print('OMG asynchronicity!')
+
+loop = asyncio.get_event_loop()
+
+'''
+The loop.run_until_complete() function is actually blocking, so it won't
+return until all of the asynchronous methods are done.
+Since we're only running this on a single thread, there is no way
+it can move forward while the loop is in progress.
+'''
+loop.run_until_complete(speak_async())
+loop.close()
+
+#Error -eg1.py:10: RuntimeWarning: coroutine 'speak_async' was never awaited
+#speak_async()
